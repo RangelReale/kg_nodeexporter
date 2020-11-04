8d9e204bd5470922c03ba135cbffd77956cab74e
Rangel Reale <rangelspam@gmail.com>
2020-11-04 17:37:39 -0300

    example

---
+++ examples/example.py
@@ -0,0 +1,86 @@
+from kubragen import KubraGen
+from kubragen.consts import PROVIDER_GOOGLE, PROVIDERSVC_GOOGLE_GKE
+from kubragen.object import Object
+from kubragen.option import OptionRoot
+from kubragen.options import Options
+from kubragen.output import OutputProject, OD_FileTemplate, OutputFile_ShellScript, OutputFile_Kubernetes, \
+    OutputDriver_Print
+from kubragen.provider import Provider
+
+from kg_nodeexporter import NodeExporterBuilder, NodeExporterOptions
+
+kg = KubraGen(provider=Provider(PROVIDER_GOOGLE, PROVIDERSVC_GOOGLE_GKE), options=Options({
+    'namespaces': {
+        'mon': 'app-monitoring',
+    },
+}))
+
+out = OutputProject(kg)
+
+shell_script = OutputFile_ShellScript('create_gke.sh')
+out.append(shell_script)
+
+shell_script.append('set -e')
+
+#
+# OUTPUTFILE: app-namespace.yaml
+#
+file = OutputFile_Kubernetes('app-namespace.yaml')
+
+file.append([
+    Object({
+        'apiVersion': 'v1',
+        'kind': 'Namespace',
+        'metadata': {
+            'name': 'app-monitoring',
+        },
+    }, name='ns-monitoring', source='app', instance='app')
+])
+
+out.append(file)
+shell_script.append(OD_FileTemplate(f'kubectl apply -f ${{FILE_{file.fileid}}}'))
+
+shell_script.append(f'kubectl config set-context --current --namespace=app-monitoring')
+
+#
+# SETUP: node-exporter
+#
+nodeexporter_config = NodeExporterBuilder(kubragen=kg, options=NodeExporterOptions({
+    'namespace': OptionRoot('namespaces.mon'),
+    'basename': 'mynodeexporter',
+    'config': {
+        'prometheus_annotation': True,
+    },
+    'kubernetes': {
+        'resources': {
+            'daemonset': {
+                'requests': {
+                    'cpu': '150m',
+                    'memory': '300Mi'
+                },
+                'limits': {
+                    'cpu': '300m',
+                    'memory': '450Mi'
+                },
+            },
+        },
+    }
+}))
+
+nodeexporter_config.ensure_build_names(nodeexporter_config.BUILD_SERVICE)
+
+#
+# OUTPUTFILE: node-exporter.yaml
+#
+file = OutputFile_Kubernetes('node-exporter.yaml')
+out.append(file)
+
+file.append(nodeexporter_config.build(nodeexporter_config.BUILD_SERVICE))
+
+shell_script.append(OD_FileTemplate(f'kubectl apply -f ${{FILE_{file.fileid}}}'))
+
+#
+# Write files
+#
+out.output(OutputDriver_Print())
+# out.output(OutputDriver_Directory('/tmp/build-gke'))
